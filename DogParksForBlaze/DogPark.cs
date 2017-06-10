cce4ae789b40a5b88a3f061581254b191ad463a8
Lisa Stuart <lisa5678@uw.edu>
2017-06-10 15:57:31 -0700

    Added Menu and Enum

--- DogParksForBlaze/DogPark.cs
+++ DogParksForBlaze/DogPark.cs
@@ -6,26 +6,93 @@ using System.Threading.Tasks;
 
 namespace DogParksForBlaze
 {
-    class DogPark
+    // enum is a way to create your own set of value types, allows user to pick from the list, coded just before and outside of class
+    // that way it can be available globally, can only put strings here with no spaces or special chars, behind the scenes
+    // assigns a number to it though (hover to see, can give it a specific value if want to), then in properties, need to
+    // change accountType from string return type to this enum name as the new return type, this restricts choices user can enter
+    // if using overload that I create instead of C# automatically, need to change return type in params to this enum name also
+    // now also need to go to Program.cs and change instantiation of 'var account' from "Checking" (highlight, space bar, gives new avail options)
+    // see second line of commented out code where replaced "Checking" with 'AccountTypes.Checking', did same for Account2
+    public enum AccountTypes
     {
+        Free,
+        HappyDog
+    }
+    /// <summary>
+    /// This is an account that holds information of an account for a particular dog owner
+    /// </summary>
+    class DogParkDateAccount
+    {
+        // static means that there's only one copy vs an instance which can be many copies of an object
+        // by making it static, the instances of 'account' can share/access the variable when necessary
+        private static int lastAccountNumber = 0;
+
+        // A. created properties:
         #region Properties
-        public string DogParkName { get; set; }
-        public string DogParkAddress { get; set; }
-        public int NumVisits { get; set; }
-        public string Rating { get; set; }
+
+        /// <summary>
+        /// Triple slash then enter gives multi-line commments; to create properties, type prop then tab, tab
+        /// accessModifier dataType NameOfProperty
+        /// </summary>
+        // private set lets you control who is able to access
+        public int AccountNumber { get; private set; } // 1. Created first, originally not private
+        public string UserName { get; set; } // 2. created second
+        public string EmailAddress { get; set; } // 3. created third
+        public string DogName { get; set; }
+        public decimal BarkBucks { get; private set; } // 4. created fourth; private means user cannot set, only those with access to this class (webmaster)
+        public AccountTypes TypeOfAccount { get; set; } // 5. created fifth, then added region specifying these are all the Properties (by naming the region that) which can then collapse
         #endregion
 
+        // C2. a constructor is a special method that doesn't require a return type and is called with the keyword 'new'
+        // it's already there, it's just hidden and only need to add to it if something else needs doing besides default
+        // the method that is created just after needs to be given the same name as the class name followed by parentheses
+        // if looking at someone else's code and see a method name that is same as class name, know it's a constructor
+        #region Constructor 
+        public DogParkDateAccount()
+        {
+            lastAccountNumber++;
+            AccountNumber = lastAccountNumber;
+        }
+
+        // D1. Overloading a constructor, using the same method with different parameters (although now C# helps so no 
+        // need to create all possible combinations (see Program.cs)
+        public DogParkDateAccount(string userName) : this() // this refers to the current construction, here the person creating
+                                                    // the new account wants a certain account name, so this overload allows to set the property as what
+                                                    // the person is telling you they want
+        {
+            UserName = userName;
+        }
+        public DogParkDateAccount(string userName, string emailAddress) : this(userName) // want to add parameter to 'this' of previous overload which in turn does previous code
+        {
+            // so if you are given an account name and email address, the last account number code is fired recursively after the one that follows it and so on (here setting email)
+            EmailAddress = emailAddress;
+        }
+        public DogParkDateAccount(string userName, string emailAddress, AccountTypes typeOfAccount) : this(userName, emailAddress)
+        {
+            TypeOfAccount = typeOfAccount;
+        }
+        public DogParkDateAccount(string userName, string emailAddress, AccountTypes typeOfAccount, decimal amount) : this(userName, emailAddress, typeOfAccount)
+        {
+            Earn(amount); // earn bark bucks based on amount of shares, spend them to set up dates or earn dog treats/toys
+        }
+        #endregion
         #region Methods
-        public string HaveVisited(string visited)
+        /// <summary>
+        /// Added this region with Methods after adding Properties methods (first) above
+        /// Adds deposit to existing balance
+        /// accessModifier returnType [void for nothing returned] NameOfMethod(dataType paramName) paramName is only
+        /// accessible within the method in which it is defined, so it's local to the scope of that method
+        /// </summary>
+        /// <param name="amount"></param>
+        public void Earn(decimal amount)
         {
-            if (NumVisits > 0)
-                return "Yes";
-            return "No";
+            BarkBucks += amount; // what does this method do? always end statements with a semi-colon (;)
         }
-        public void VisitPark(int currentVisit)
+        public void Use(decimal amount)
         {
-            NumVisits += 1;
+            BarkBucks -= amount;
         }
+
         #endregion
     }
 }
--- DogParksForBlaze/Program.cs
+++ DogParksForBlaze/Program.cs
@@ -10,6 +10,61 @@ namespace DogParksForBlaze
     {
         static void Main(string[] args)
         {
+            // E. code a menu to display choices to user
+            Console.WriteLine("************Welcome to Doggy Play Date******************");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Create a new account");
+            //Console.WriteLine("2. ");
+            //Console.WriteLine("3. ");
+            //Console.WriteLine("4. ");
+
+            // ask user to select something from list
+            // readline puts cursor on screen and stays until user types a selection and presses enter
+            var choice = Console.ReadLine(); // takes user input and stores as variable called 'choice'
+
+            // if checking for equality, instead of using if/else if stmt, use a switch (explanation on video from
+            // 5/21 at 39 minutes); if need to check > or <, need to use if/else stmt
+            switch (choice)
+            {
+                case "0":
+                    return;
+                case "1":
+                    Console.Write("User Name: "); // .Write instead of writeLine keeps cursor on same line instead of moving to next line
+                    var userName = Console.ReadLine();
+                    Console.Write("Email Address: ");
+                    var emailAddress = Console.ReadLine();
+                    Console.Write("Dog's Name: ");
+                    var dogName = Console.ReadLine();
+                    Console.WriteLine("Type of Account: ");
+                    var accountTypes = Enum.GetNames(typeof(AccountTypes)); // this sets up ability to add an account to enum so is dynamic
+                    // so this tells Enum to get the names of account types by passing the enum name of 'AccountTypes' as a param to typeof method
+                    // if hover over 'GetNames' tool tip has string[] indicating giving back string array of AccountTypes given to variable names accountTypes
+                    for (int i = 0; i < accountTypes.Length; i++) // for tab tab; for from 0 to length of accountTypes variable array
+                    {
+                        Console.WriteLine($"{i}. {accountTypes[i]}"); // curly braces prints what is inside it, the index num first and then the value at that index
+                    }
+                    Console.Write("Pick an account type: ");
+                    var accountType = Convert.ToInt32(Console.ReadLine()); // b/c ReadLine gives a string, need to use convert function
+                    Console.Write("Amount: ");
+                    var amount = Convert.ToDecimal(Console.ReadLine());
+
+                    // now that have everything from user, time to actually create the account by passing what user gave
+                    var dogParkDateAccount = new DogParkDateAccount
+                    {
+                        UserName = userName,
+                        EmailAddress = emailAddress,
+                        TypeOfAccount = (AccountTypes)accountType // type conversion by using name want to custom convert to (explanation
+                        // in 5/21 video at 37 minutes)
+                    };
+                    dogParkDateAccount.Earn(amount);
+                    Console.WriteLine($"AN: , {dogParkDateAccount.AccountNumber}, BarkBucks Balance: {dogParkDateAccount.BarkBucks}");
+                    break; // when done with this case, need to get out of it, otherwise will see squiggly line under 'Case 1' above
+                //case "2":
+                //case "3":
+                //case "4":
+                //default:
+                    //break;
+            }
         }
     }
 }
